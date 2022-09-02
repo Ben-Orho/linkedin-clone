31dcab55f495257a741f732487ae56809763ee7b
Ben-Orho <heyjis4connection@gmail.com>
2022-09-02 04:20:43 +0100

    touching

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -13,17 +13,17 @@ function Sidebar() {
         <Avatar className="sidebar__avatar" />
         <h2>Ben Orho</h2>
         <h4>benorho@gmail.com</h4>
+        <p> student at the university of ibadan </p>
       </div>
 
       <div className="sidebar__stats">
-        <p> student at the university of lagos </p>
         <div className="sidebar__stat">
           <p className="sidebar__statconnection">connections</p>
           <p className="sidebar__statnumber">2,406</p>
         </div>
-        <h5>connect with alumini</h5>
       </div>
       <div className="sidebar__stat">
+        <h5>connect with alumini</h5>
         <p>invitations</p>
       </div>
     </div>
